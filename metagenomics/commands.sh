521fe65a5ade56a01632cd671bc1edf37867354f
Artem Burtsev <86663451+prometneus@users.noreply.github.com>
2021-11-17 19:24:44 +0300

    Create commands.sh

---
+++ metagenomics/commands.sh
@@ -0,0 +1,8 @@
+sudo apt install fastqc
+fastqc combined*.fq 
+conda install -c bioconda -c conda-forge multiqc
+multiqc -o multiqc *.downsampled_fastqc.zip
+wget http://cab.spbu.ru/files/release3.15.3/SPAdes-3.15.3-Linux.tar.gz
+tar -xzf SPAdes-3.15.3-Linux.tar.gz
+cd SPAdes-3.15.3-Linux/bin/
+metaspades --only-assembler -1 combined_1_val_1.downsampled.fq -2 combined_2_val_2.downsampled.fq -o metaspades
