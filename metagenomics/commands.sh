67e773d4d3656f71cccb17cc6a1f00c264849a79
Artem Burtsev <86663451+prometneus@users.noreply.github.com>
2021-11-20 16:52:27 +0300

    Update commands.sh

--- metagenomics/commands.sh
+++ metagenomics/commands.sh
@@ -6,6 +6,8 @@ wget http://cab.spbu.ru/files/release3.15.3/SPAdes-3.15.3-Linux.tar.gz
 tar -xzf SPAdes-3.15.3-Linux.tar.gz
 cd SPAdes-3.15.3-Linux/bin/
 metaspades --only-assembler -1 combined_1_val_1.downsampled.fq -2 combined_2_val_2.downsampled.fq -o metaspades
-**Качаю QUAST с GitHub`а**
+**Качаю QUAST с GitHubа**
 bash install_full.sh
 ./metaquast.py -r ../refs/*.fna -o ../metaquast_output/ ../metaspades/contigs.fasta
+metabat2 -i ./GenomeAssembly/metaspades/contigs.fasta -o ./GenomeAssembly/metabat_output/file
+./metaquast.py -t 16 ../metabat_output/*.fa -o ../metaquast_output/report1
